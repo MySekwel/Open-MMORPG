0fa6685b9f746307cf769fb017b4e61fb54eb27a
MySekwel <mysekwel@gmail.com>
2021-02-10 23:36:04 +0800

    Setting Up General Commands

--- bot/cogs/g_commands.py
+++ bot/cogs/g_commands.py
@@ -10,16 +10,20 @@ class GeneralCommands(commands.Cog):
 
     @commands.group(invoke_without_command=True)
     async def get(self):
-        pass
+        print('Test')
 
     @get.command()
     async def ip(self, ctx, name):
         if not user.Registered(name):
-            await notif.SendError(ctx, "Cannot find user from the database!", tip="Make sure the name is correct.")
+            await notif.SendError(ctx, 'Cannot find user from the database!', tip='Make sure the name is correct.')
             return
         user_id = getter.GetUIDFromUserName(name)
         ip = getter.GetIPFromUserUID(user_id)
         await ctx.send(f'The **IP Address** of the name `{name}` is `{ip}`.')
+
+    @commands.command()
+    async def help(self, ctx):
+        await ctx.author.send('HEEEEEEEEEEEEEEEELP!')
 
 
 def setup(bot):
---
+++ bot/utils/emojii.py
@@ -0,0 +1,35 @@
+def GetEmoji(emoji):
+    return emojis[f'{emoji}']
+
+
+emojis = {
+    'face_grinning': '\U00000000',
+    'face_smiley': '\U00000000',
+    'face_smile': '\U00000000',
+    'face_grin': '\U00000000',
+    'face_laughing': '\U00000000',
+    'face_satisfied': '\U00000000',
+    'face_sweet_smile': '\U00000000',
+    'face_joy': '\U00000000',
+    'face_rofl': '\U00000000',
+    'face_relaxed': '\U00000000',
+    'face_blush': '\U00000000',
+    'face_innocent': '\U00000000',
+    'face_slight_smile': '\U00000000',
+    'face_upside_down': '\U00000000',
+    'face_wink': '\U00000000',
+    'face_relieved': '\U00000000',
+    'face_smiling_with_tear': '\U00000000',
+    'face_heart_eyes': '\U00000000',
+    'face_smiling_with_3_hearts': '\U00000000',
+    'face_kissing_heart': '\U00000000',
+    'face_kissing': '\U00000000',
+    'face_kissing_smiling_eyes': '\U00000000',
+    'face_closed_eyes': '\U00000000',
+    'face_yum': '\U00000000',
+    'face_stuck_out_tongue': '\U00000000',
+    'face_stuck_out_tongue_closed_eyes': '\U00000000',
+    'face_stuck_out_tongue_winking_eye': '\U00000000',
+    'face_zany': '\U00000000',
+
+}
